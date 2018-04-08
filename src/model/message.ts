aa1fd115c4e2b3b04f5c24acdc7f8ed246a1dbff
Matias Iordache <matias.iordache@gmail.com>
2018-04-08 16:04:43 +0200

    add coments to models

--- src/model/message.ts
+++ src/model/message.ts
@@ -1,5 +1,11 @@
 import { User } from "./user";
 
+/**
+ * Chat message class, has a from user and content string
+ * 
+ * @export
+ * @class ChatMessage
+ */
 export class ChatMessage {
     constructor(public from: User, public content: string) {}
 }
--- src/model/user.ts
+++ src/model/user.ts
@@ -1,3 +1,9 @@
+/**
+ * User class, has a name and an id (socket id)
+ * 
+ * @export
+ * @class User
+ */
 export class User {
     constructor(public id: number, public name: string) {}
 }
