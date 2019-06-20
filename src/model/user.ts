19823d0c41372b644d6fa2095bb1fcb79c882472
Matias Iordache <matias.iordache@gmail.com>
2019-06-20 18:42:51 +0200

    implement private message pass all messages on join

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,9 +1,9 @@
 /**
  * User class, has a name and an id (socket id)
- * 
+ *
  * @export
  * @class User
  */
 export class User {
-    constructor(public id: number, public name: string, public image: string) {}
+  constructor(public id: string, public name: string, public image: string) {}
 }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import { createServer, Server } from "http";
-import * as express from "express";
-import * as socketIo from "socket.io";
+import * as express from 'express';
+import { createServer, Server } from 'http';
+import * as socketIo from 'socket.io';
 
-import { ChatMessage } from "./model/message";
-import { User } from "./model/user";
+import { ChatMessage } from './model/message';
+import { User } from './model/user';
 
 /**
  * Chat server class
- * 
+ *
  * @export
  * @class ChatServer
  */
@@ -17,8 +17,9 @@ export class ChatServer {
   private server: Server;
   private io: socketIo.Server;
   private port: string | number;
-  
+
   private users: User[] = [];
+  private messages: ChatMessage[] = [];
 
   /**
    * Creates an instance of ChatServer.
@@ -34,13 +35,13 @@ export class ChatServer {
 
   /**
    * Server listens to port
-   * 
+   *
    * @private
    * @memberof ChatServer
    */
   private listen(): void {
     this.server.listen(this.port, () => {
-      console.log("Server listening on port %s", this.port);
+      console.log('Server listening on port %s', this.port);
     });
 
     this.connect();
@@ -48,90 +49,135 @@ export class ChatServer {
 
   /**
    * Handles new connection
-   * 
+   *
    * @private
    * @memberof ChatServer
    */
   private connect() {
-    this.io.on("connect", (socket: any) => {
-      socket.broadcast.emit("join", "Someone joined the room");
+    this.io.on('connect', (socket: socketIo.Socket) => {
+      socket.broadcast.emit('join', 'Someone joined the room');
+      socket.emit('messages', this.messages);
       console.log(`Client ${socket.id} connected on port ${this.port}.`);
       this.rename(socket);
       this.join(socket);
       this.message(socket);
+      this.privateMessage(socket);
       this.disconnect(socket);
     });
   }
 
   /**
    * Handles new message
-   * 
+   *
    * @private
-   * @param {*} socket 
+   * @param {*} socket
    * @memberof ChatServer
    */
-  private message(socket: any) {
-    socket.on("message", (message: any) => {
-      console.log("[server](message): %s", JSON.stringify(message));
-      this.io.emit("message", message);
+  private message(socket: socketIo.Socket) {
+    socket.on('message', (message: any) => {
+      console.log('[server](message): %s', JSON.stringify(message));
+      this.messages.push(message);
+      this.io.emit('message', message);
+    });
+  }
+
+  /**
+   * Handles PM
+   *
+   * @private
+   * @param {*} socket
+   * @memberof ChatServer
+   */
+  private privateMessage(socket: socketIo.Socket) {
+    socket.on('privateMessage', (id: string, message: ChatMessage) => {
+      console.log(
+        '[server](private message): %s to %s',
+        JSON.stringify(message),
+        id
+      );
+      this.io.to(id).emit('privateMessage', message);
     });
   }
 
   /**
    * Handles when a user disconect, removes user from array
-   * 
+   *
    * @private
-   * @param {*} socket 
+   * @param {*} socket
    * @memberof ChatServer
    */
-  private disconnect(socket: any) {
-    socket.on("disconnect", () => {
+  private disconnect(socket: socketIo.Socket) {
+    socket.on('disconnect', () => {
       console.log(`Client ${socket.id} disconnected on port ${this.port}.`);
       const index = this.users.findIndex(user => user.id === socket.id);
       if (index !== -1) {
         this.users.splice(index, 1);
-        this.io.emit("users", this.users);
+        this.io.emit('users', this.users);
       }
     });
   }
 
   /**
    * Handles user rename, modifies name from user array
-   * 
+   *
    * @private
-   * @param {*} socket 
+   * @param {*} socket
    * @memberof ChatServer
    */
-  private rename(socket: any) {
-    socket.on("rename", (data: any) => {
-      console.log("[server](rename) user renamed %s", JSON.stringify(data));
+  private rename(socket: socketIo.Socket) {
+    socket.on('rename', (data: any) => {
+      console.log('[server](rename) user renamed %s', JSON.stringify(data));
       const index = this.users.findIndex(user => user.id === data.user.id);
       if (index !== -1) {
         this.users[index].name = data.user.name;
-        this.io.emit("users", this.users);
+        this.io.emit('users', this.users);
       }
     });
   }
   /**
    * Handles when a new user joins chat
-   * 
+   *
    * @private
-   * @param {*} socket 
+   * @param {*} socket
    * @memberof ChatServer
    */
-  private join(socket: any) {
-    socket.on("join", (data: any) => {
-      console.log("[server](join) %s joined", JSON.stringify(data));
-      socket.emit("id", socket.id);
-      this.users.push({ id: socket.id, name: data.from.name, image: data.from.image });
-      this.io.emit("users", this.users);
+  private join(socket: socketIo.Socket) {
+    socket.on('join', (data: any) => {
+      console.log('[server](join) %s joined', JSON.stringify(data));
+      const newUser = this.getNewUser(socket.id);
+      socket.emit('user', newUser);
+      this.users.push(newUser);
+      this.io.emit('users', this.users);
     });
   }
 
   /**
+   *
+   *
+   * @private
+   * @param {string} id
+   * @returns {User}
+   * @memberof ChatServer
+   */
+  private getNewUser(id: string): User {
+    return { id: id, name: 'Anonymous', image: this.getRandomImage() };
+  }
+
+  /**
+   *
+   *
+   * @returns {string}
+   * @memberof ChatServer
+   */
+  private getRandomImage(): string {
+    const collectionId = Math.floor(Math.random() * 100000) + 1;
+    return `https://source.unsplash.com/random?sig=${collectionId}`;
+  }
+
+  /**
    * Returns app
-   * 
-   * @returns {express.Application} 
+   *
+   * @returns {express.Application}
    * @memberof ChatServer
    */
   public getApp(): express.Application {
